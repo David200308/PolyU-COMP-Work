473a2dbf8c6c1fc046a58d0d3b0d7437688c4dfb
David Jiang <45353198+David200308@users.noreply.github.com>
2022-04-07 08:36:38 -0700

    Add files via upload

--- COMP1433/Assignment/Q1/q1.R
+++ COMP1433/Assignment/Q1/q1.R
@@ -1,43 +1,24 @@
-include_agrippa <- function(seed) {
-  set.seed(seed)
-  agrippa <- round(runif(1, 0, 1))
-  return (agrippa)
-}
+cards <- c("Agrippa", "Ptolemy", "Others")
+prob <- c(0.01, 0.01, 0.98)
+N = 100000
 
-include_ptolemy <- function(seed) {
-  set.seed(seed)
-  ptolemy <- round(runif(1,0,1))
-  return(ptolemy)
-}
-
-simulate <- function(N, seed){
-  set.seed(seed)
-  x <- numeric()
-  y <- numeric()
-  while (i <= N) {
-    if (i %in% 1:N) {
-      agrippa = include_agrippa(i)
-      ptolemy = include_ptolemy(i)
-      cards <- round(runif(1, 0, 500))
-      if(agrippa == 0 & ptolemy == 0 & cards == 500){
-        return(1)
-        }
-         }
-         i = i + 1
-     }
-  return(0)
-}
+classes_r <- data.frame(a_and_p = 0, a_or_p = 0, n_a_p = 0)
+names(classes_r) <- c("A and P", "A or P", "Not A and P")
 
 i <- 1
-while (i <= 1000) {
-  count <- 0
-  j <- 1
-  while (j <= i) {
-    values <- simulate(100,j)
-    count <- count + values
-    j = j + 1
+while (i <= N) {
+  t <- table(sample(cards, 500, replace = TRUE, prob))
+  if (length(t) == 3) {
+    classes_r$"A and P" = classes_r$"A and P" + 1
+  } else if (length(t) == 2) {
+    classes_r$"A or P" = classes_r$"A or P" + 1
+  }else {
+    classes_r$"Not A and P" = classes_r$"Not A and P" + 1
   }
+  
   i = i + 1
 }
-result = count / i
+
+result = ((classes_r$"Not A and P")/ N)* prob[3]
+print("The result is: ")
 print(result)
